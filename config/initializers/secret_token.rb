1c199ef37bedd522627a925476d12af553a96f49
shagmbg <michael.b.gottfried@gmail.com>
2013-12-17 14:39:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ruumies::Application.config.secret_key_base = '0eb84cd45695dbd54b45e60283a7987de3a9d8ed888e3c78af41a0e012810d523098ec95a0308e3b5fa9ad3e9ee64bd9a89ea131e1f51adbf4facd867573af05'
