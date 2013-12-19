ea0c3295a14928469678802c166b4b68a0e0bb3a
shagmbg <michael.b.gottfried@gmail.com>
2013-12-19 11:53:05 -0500

    hash is working in sidebar... sort of

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218164750) do
+ActiveRecord::Schema.define(version: 20131219164644) do
 
   create_table "houses", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20131218164750) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "house_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
